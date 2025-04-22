43eb984b7677e35bf099af066b593c62564250b3
rscarberry-wa <drrandys@yahoo.com>
2025-04-22 11:30:00 -0400

    Put AlphaSquared into __init__.py

--- coinbase_advanced_trader/__init__.py
+++ coinbase_advanced_trader/__init__.py
@@ -1,4 +1,5 @@
 from .enhanced_rest_client import EnhancedRESTClient
 from .alphasquared_trader import AlphaSquaredTrader
+from .alphasquared import AlphaSquared
 
-__all__ = ['EnhancedRESTClient', 'AlphaSquaredTrader']
+__all__ = ['EnhancedRESTClient', 'AlphaSquaredTrader', 'AlphaSquared']
